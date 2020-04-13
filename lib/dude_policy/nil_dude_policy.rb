c9afcc742db94605304707cc41aeef13d5c907c3
Tomas Valent <equivalent@eq8.eu>
2020-04-13 11:39:05 +0200

    respond_to_overide

--- lib/dude_policy/nil_dude_policy.rb
+++ lib/dude_policy/nil_dude_policy.rb
@@ -6,6 +6,12 @@ module DudePolicy
       false
     end
 
+    # test frameworks like RSpec test first if method responds to before calling it
+    # as this is a NullObject delegator it responds to any method call
+    def respond_to?(*)
+      true
+    end
+
     def inspect
       "<#DudePolicy##{object_id} on nil>"
     end
