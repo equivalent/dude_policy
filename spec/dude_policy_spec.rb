7448a262cfe7e8f43fe0d0ff406443ce508c62c9
Tomas Valent <equivalent@eq8.eu>
2023-10-22 21:22:44 +0200

    nil extension test

--- spec/dude_policy_spec.rb
+++ spec/dude_policy_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe DudePolicy do
 
   it "nil should be extended with NilDudePolicy" do
     expect(nil.dude).to be_kind_of DudePolicy::NilDudePolicy
-
     expect(nil.dude.inspect).to match /<#DudePolicy#\d* on nil>/
+
+    expect(nil.policy).to be_kind_of DudePolicy::NilDudePolicy
+
+    expect(nil.dude.whatever).to be false
+    expect(nil.policy.whatever).to be false
   end
 end
